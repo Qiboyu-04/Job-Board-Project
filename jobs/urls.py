6e690f727450c2b849bf8ffae97dfc06280b3c47
Zzs77a <2317531163@qq.com>
2026-03-20 02:29:42 +1300

    Add apply feature（实现职位申请功能），支持重复申请检测、成功提示，并完善Job Detail页面

--- jobs/urls.py
+++ jobs/urls.py
@@ -12,6 +12,9 @@ urlpatterns = [
 
     path('jobs/', views.job_list),
 
+    path('jobs/<int:job_id>/', views.job_detail, name='job_detail'),
+
+    path('jobs/<int:job_id>/apply/', views.apply_job, name='apply_job'),
 
 ]
 
--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
-from .models import Job
+from .models import Job, Application
+from .forms import ApplicationForm
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
@@ -16,6 +18,44 @@ def job_list(request):
     })
 
 def job_detail(request, job_id):
-    
     job = get_object_or_404(Job, pk=job_id)
-    return render(request, 'jobs/job_detail.html', {'job': job})
+
+    already_applied = False
+    if request.user.is_authenticated:
+        already_applied = Application.objects.filter(
+            student=request.user,
+            job=job
+        ).exists()
+
+    success = request.GET.get('success') == '1'
+    duplicate = request.GET.get('duplicate') == '1'
+
+    return render(request, 'jobs/job_detail.html', {
+        'job': job,
+        'already_applied': already_applied,
+        'success': success,
+        'duplicate': duplicate,
+    })
+
+@login_required
+def apply_job(request, job_id):
+    job = get_object_or_404(Job, pk=job_id)
+
+    if Application.objects.filter(student=request.user, job=job).exists():
+        return redirect(f'/jobs/{job.id}/?duplicate=1')
+
+    if request.method == 'POST':
+        form = ApplicationForm(request.POST)
+        if form.is_valid():
+            app = form.save(commit=False)
+            app.student = request.user
+            app.job = job
+            app.save()
+            return redirect(f'/jobs/{job.id}/?success=1')
+    else:
+        form = ApplicationForm()
+
+    return render(request, 'jobs/apply.html', {
+        'form': form,
+        'job': job
+    })
